83ce6112e8bc9fad8d16f3fce5616d981aa5877f
Alexander Gerbik <alexander.gerbik@gmail.com>
2018-08-08 14:46:19 +0300

    Add functions for finding applicable gift cards and product price schedules

--- smilewidgets/products/models.py
+++ smilewidgets/products/models.py
@@ -1,33 +1,50 @@
 from django.db import models
+from django.db.models import Q
 
 
 class Product(models.Model):
     name = models.CharField(max_length=25, help_text='Customer facing name of product')
     code = models.CharField(max_length=10, help_text='Internal facing reference to product')
     price = models.PositiveIntegerField(help_text='Price of product in cents')
-    
+
     def __str__(self):
         return '{} - {}'.format(self.name, self.code)
 
 
-class GiftCard(models.Model):
-    code = models.CharField(max_length=30)
-    amount = models.PositiveIntegerField(help_text='Value of gift card in cents')
+class Interval(models.Model):
+    # both date_start and date_end are inclusive
     date_start = models.DateField()
     date_end = models.DateField(blank=True, null=True)
-    
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def find_applicable(model, date):
+        return model.objects.filter(
+            Q(date_start__lte=date)
+            & (Q(date_end__isnull=True) | Q(date_end__gte=date))
+        )
+
+    def is_applicable(self, date):
+        return (self.date_start <= date
+                and (self.date_end is None or date <= self.date_end))
+
+
+class GiftCard(Interval):
+    code = models.CharField(max_length=30)
+    amount = models.PositiveIntegerField(help_text='Value of gift card in cents')
+
     def __str__(self):
         return '{} - {}'.format(self.code, self.formatted_amount)
-    
+
     @property
     def formatted_amount(self):
         return '${0:.2f}'.format(self.amount / 100)
 
-class ProductPriceSchedule(models.Model):
+
+class ProductPriceSchedule(Interval):
     name = models.CharField(max_length=25, help_text='Price schedule description')
-    # both date_start and date_end are inclusive
-    date_start = models.DateField()
-    date_end = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return '{} - {}-{}'.format(self.name, self.date_start, self.date_end or '∞')
--- smilewidgets/products/tests.py
+++ smilewidgets/products/tests.py
@@ -1,3 +1,54 @@
+from datetime import date
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import ProductPriceSchedule, GiftCard
+
+
+class ContainingIntervalTestCase(TestCase):
+    fixtures = ['0001_fixtures.json']
+
+    def test_product_price_schedule(self):
+        cases = (
+            (date(2018, 5, 5), []),
+            (date(2018, 11, 22), []),
+            (date(2018, 11, 23), ['<ProductPriceSchedule: Black Friday Prices - 2018-11-23-2018-11-25>']),
+            (date(2018, 11, 25), ['<ProductPriceSchedule: Black Friday Prices - 2018-11-23-2018-11-25>']),
+            (date(2018, 11, 26), []),
+            (date(2018, 12, 31), []),
+            (date(2019, 1, 1), ['<ProductPriceSchedule: 2019 Prices - 2019-01-01-∞>']),
+        )
+        for dt, expected in cases:
+            with self.subTest(date=dt, expected=expected):
+                actual = ProductPriceSchedule.find_applicable(dt)
+                self.assertQuerysetEqual(actual, expected, ordered=False)
+
+    def test_gift_card(self):
+        cases = (
+            (date(2018, 6, 30), []),
+            (date(2018, 7, 1), [
+                '<GiftCard: 10OFF - $10.00>',
+                '<GiftCard: 50OFF - $50.00>',
+            ]),
+            (date(2018, 11, 23), [
+                '<GiftCard: 10OFF - $10.00>',
+                '<GiftCard: 50OFF - $50.00>',
+            ]),
+            (date(2018, 11, 30), [
+                '<GiftCard: 10OFF - $10.00>',
+                '<GiftCard: 50OFF - $50.00>',
+            ]),
+            (date(2018, 12, 31), [
+                '<GiftCard: 10OFF - $10.00>',
+                '<GiftCard: 50OFF - $50.00>',
+                '<GiftCard: 250OFF - $250.00>',
+            ]),
+            (date(2019, 1, 2), [
+                '<GiftCard: 10OFF - $10.00>',
+                '<GiftCard: 50OFF - $50.00>',
+            ]),
+        )
+        for dt, expected in cases:
+            with self.subTest(date=dt, expected=expected):
+                actual = GiftCard.find_applicable(dt)
+                self.assertQuerysetEqual(actual, expected, ordered=False)
